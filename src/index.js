eddeb1d7084f7d2052226b1dc47c0c32ba0f5e5e
Stephanie Anderson <srglover126@gmail.com>
2025-01-15 15:27:21 -0500

    added basic form handling and typewriter font

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "There once was a rollercoaster named Bob",
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
